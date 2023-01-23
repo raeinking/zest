e4852aca2c5e7a48e3c620d2c773a58a4e87829e
unknown <raeinking2@gmail.com>
2023-01-23 10:35:40 +0300

    commit

--- src/components/projects/alwedd/Alwedd.jsx
+++ src/components/projects/alwedd/Alwedd.jsx
@@ -211,6 +211,17 @@ const Alwedd = () => {
             </a>
             </div>
           </form>
+          <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={'https://www.youtube.com/embed/KV2QCUpB0Zc'}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    ></iframe>
+  </div>
     </main>
       </>
     :
@@ -382,6 +393,17 @@ const Alwedd = () => {
             </a>
             </div>
           </form>
+          <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={'https://www.youtube.com/embed/KV2QCUpB0Zc'}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    ></iframe>
+  </div>
     </main>
     </>
     }
--- src/components/projects/khlood/khlood.jsx
+++ src/components/projects/khlood/khlood.jsx
@@ -156,6 +156,17 @@ const Khlood = () => {
             </a>
             </div>
           </form>
+            <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={'https://www.youtube.com/embed/Av9ZM_dpKuM'}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    ></iframe>
+  </div>
     </main>
       </>
     :
@@ -260,6 +271,17 @@ const Khlood = () => {
             </a>
             </div>
           </form>
+          <div className="video-responsive">
+    <iframe
+      width="853"
+      height="480"
+      src={'https://www.youtube.com/embed/Av9ZM_dpKuM'}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      title="Embedded youtube"
+    ></iframe>
+  </div>
     </main>
     </>
     }
