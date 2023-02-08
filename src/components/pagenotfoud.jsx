0d19500a628297ccf7a6dd64168a204ccccd7afc
unknown <raeinking2@gmail.com>
2023-02-08 13:23:28 +0300

    commit

--- src/components/pagenotfoud.jsx
+++ src/components/pagenotfoud.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function pagenotfoud() {
   return (
     <div className='pagenotfound'>
-      this page not avalabil go to this link
+      this page not avalabil go to this link <a href='/'>Home</a>
     </div>
   )
 }
